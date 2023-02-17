b948e63d361a6b35d4ebe8faf8449e3272bfafea
DavidFFDAW <davidferflo2@gmail.com>
2023-02-17 14:28:55 +0100

    feat: custom web push notification worker

--- worker/index.js
+++ worker/index.js
@@ -5,7 +5,7 @@ self.addEventListener("push", function (event) {
     event.waitUntil(
         registration.showNotification(data.title, {
             body: data.message,
-            icon: "/icons/android-chrome-192x192.png",
+            icon: "/icons/icons-192x192.png",
         })
     );
 });
