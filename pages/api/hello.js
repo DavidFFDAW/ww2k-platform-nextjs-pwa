1b4a873fcdfaed8c3942708056be7f4ad1e9969b
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-03-14 10:04:08 +0100

    chore: test implementations on hello route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { UserAdapters } from "@/adapters/user.adapter";
+import { APIService } from "@/services/api.service"
+
+// http://localhost:3000/api/hello
+export default async function handler(req, res) {
+  const service = APIService(req, res);
+
+  service.checkAPIMethod('GET');
+  const user = await service.checkValidHeaderToken();
+  console.log('user', user);
+
+  if (user) {
+    res.status(200).json(UserAdapters.publicDataAdapter(user));
+  }
 }
