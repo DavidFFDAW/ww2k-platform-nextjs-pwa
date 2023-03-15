8bdf7af1975cf48630ede3283c1628df008facf1
DavidFFDAW <davidferflo2@gmail.com>
2023-03-15 20:05:21 +0100

    chore: non-important changes

--- middleware.js
+++ middleware.js
@@ -1,13 +1,21 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) { 
+export function middleware(request) {
     const { pathname } = request.nextUrl;
-    console.log('Path:', pathname);
+    const cookies = request.cookies;
+    // console.log("Request:", request);
+    // console.log("Path:", pathname);
+    console.log(
+        cookies.get("remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d")
+    );
 
-    if (pathname.startsWith('/admin')) {
-        return NextResponse.redirect(new URL('/auth/login', request.url).toString(), { status: 307 });
+    if (pathname.startsWith("/admin")) {
+        return NextResponse.redirect(
+            new URL("/auth/login", request.url).toString(),
+            { status: 307 }
+        );
     }
-    
+
     return NextResponse.next();
 }
 
@@ -20,6 +28,6 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico|workbox-*.js|icons/*|worker-development.js|manifest.json|sw.js).*)',
+        "/((?!api|_next/static|_next/image|favicon.ico|workbox-*.js|icons/*|worker-development.js|manifest.json|sw.js).*)",
     ],
 };
