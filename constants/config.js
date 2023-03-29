983a20f9bfd8d3afbcb9a39c48eae6176ad0897e
DavidFFDAW <davidferflo2@gmail.com>
2023-03-30 01:57:23 +0200

    refactor: change environment back to production

--- constants/config.js
+++ constants/config.js
@@ -1,10 +1,10 @@
-const APPENV = 'development'; // || 'product';
+const APPENV = 'prod'; // dev or prod
 
 const config = {
     NEXT_AUTH_KEY: 'next-auth-key',
     NEXT_USER: 'next-user-object',
     HTTP_REQUEST_URL_API:
-        APPENV === 'development'
+        APPENV === 'dev'
             ? 'http://localhost:3000/api'
             : 'https://ww2k-platform-nextjs-pwa.vercel.app/api',
 };
