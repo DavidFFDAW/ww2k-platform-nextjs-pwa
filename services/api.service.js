0ed8aa988fb5169f874e97ccd747feece6954114
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-03-30 08:30:30 +0200

    refactor: server API part

--- services/api.service.js
+++ services/api.service.js
@@ -3,7 +3,8 @@ import UserTable from "@/database/User";
 export const APIService = (req, res) => {
     return {
         checkAPIMethod: (method = 'GET') => {
-            if (req.method !== method) {
+            const methods = [method,'PATCH', 'OPTIONS'];
+            if (!methods.includes(req.method)) {
                 return res.status(405).json({ message: `Method ${req.method} not allowed` });
             }
         },
