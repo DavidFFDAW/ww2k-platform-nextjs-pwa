38cc3282ee13ae5fa7340cd57fabc624da23674e
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-03-30 12:25:57 +0200

    refactor: mysql dialect manually setted

--- database/Connection.js
+++ database/Connection.js
@@ -1,9 +1,11 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
 const env = process.env;
 const Connection = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
     host: env.DB_HOST,
     dialect: "mysql",
+    dialectModule: mysql2, 
     logging: false,
 });
 
