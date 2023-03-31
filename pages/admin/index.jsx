222516ed00b7c54c92e03a3f5d283d2673e9727b
DavidFFDAW <davidferflo2@gmail.com>
2023-04-01 00:50:36 +0200

    feat: add logout button

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -5,11 +5,33 @@ import { SiWwe } from 'react-icons/si';
 import { GiChampions, GiTrophy } from 'react-icons/gi';
 import { RiTeamFill } from 'react-icons/ri';
 import CustomImage from '@/components/Image';
+import CookieService from '@/services/cookie.service';
+import config from '@/constants/config';
+import { useRouter } from 'next/router';
 
-export default function index() {
+export default function AdminHomeDashboard() {
     const imagesize = 152;
+
+    const router = useRouter();
+    const logOut = () => {
+        console.log('LOGOUT');
+        CookieService.remove(config.NEXT_USER);
+        router.replace('/login');
+    };
+
     return (
         <BackgroundPage>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    right: '15px',
+                }}
+            >
+                <button class="btn btn-app" onClick={logOut()}>
+                    Log Out
+                </button>
+            </div>
             <div className="w1 flex center al-center h1">
                 <div className="w1 flex end column al-center h1 home-spas-links gap-small">
                     <CustomImage
