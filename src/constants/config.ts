ad85196178dff3590beab7a462c741b66a1cf1c5
DavidFFDAW <davidferflo2@gmail.com>
2023-10-16 15:01:12 +0200

    feat: pages, auth and middleware

---
+++ src/constants/config.ts
@@ -0,0 +1,18 @@
+
+export const USER_ROLES = {
+    ADMIN: "ADMIN",
+    USER: "USER",
+};
+
+export const PROTECTED_ROUTES = {
+    ADMIN: "/admin",
+    USER: "/user",
+};
+
+export const STATIC_PAGES = {
+    ABOUT: "/about",
+    CONTACT: "/contact",
+    PRIVACY: "/privacy",
+    TERMS: "/terms",
+    DENIED: "/denied",
+};
---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
+import { PROTECTED_ROUTES, STATIC_PAGES, USER_ROLES } from '@/constants/config';
+
+export default withAuth(
+    function middleware(request: NextRequestWithAuth) {
+        const authRole = request.nextauth.token?.user as {
+            role: string;
+        };
+
+        const userRole = authRole?.role.toUpperCase();
+
+        if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.ADMIN) && userRole !== USER_ROLES.ADMIN) {
+            return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
+        }
+
+        if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.USER) && userRole !== USER_ROLES.USER) {
+            return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
+        }
+    },
+    {
+        callbacks: {
+            authorized: ({ token }) => !!token,
+        },
+    },
+);
+
+export const config = {
+    matcher: ['/admin/:path*', '/user/:path'],
+};
