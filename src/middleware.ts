77ef554b6aa96dbc706d70714e925e37474592f1
DavidFFDAW <davidferflo2@gmail.com>
2023-10-22 23:07:57 +0200

    refactor: enabled middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,30 @@
-import { NextRequestWithAuth } from 'next-auth/middleware';
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
-// import { PROTECTED_ROUTES, STATIC_PAGES, USER_ROLES } from '@/constants/config';
+import { PROTECTED_ROUTES, STATIC_PAGES, USER_ROLES } from '@/constants/config';
 
+export default withAuth(
+    function middleware(request: NextRequestWithAuth) {
+        const authRole = request.nextauth.token?.user as {
+            role: string;
+        };
 
-export default function middleware(request: NextRequestWithAuth) {
-    return NextResponse.next();
-}
+        const userRole = authRole?.role.toUpperCase();
 
+        if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.ADMIN) && userRole !== USER_ROLES.ADMIN) {
+            return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
+        }
 
-// export default withAuth(
-//     function middleware(request: NextRequestWithAuth) {
-//         const authRole = request.nextauth.token?.user as {
-//             role: string;
-//         };
+        if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.USER) && userRole !== USER_ROLES.USER) {
+            return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
+        }
+    },
+    {
+        callbacks: {
+            authorized: ({ token }) => !!token,
+        },
+    },
+);
 
-//         const userRole = authRole?.role.toUpperCase();
-
-//         if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.ADMIN) && userRole !== USER_ROLES.ADMIN) {
-//             return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
-//         }
-
-//         if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.USER) && userRole !== USER_ROLES.USER) {
-//             return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
-//         }
-//     },
-//     {
-//         callbacks: {
-//             authorized: ({ token }) => !!token,
-//         },
-//     },
-// );
-
-// export const config = {
-//     matcher: ['/admin/:path*', '/user/:path'],
-// };
+export const config = {
+    matcher: ['/admin/:path*', '/user/:path'],
+};
