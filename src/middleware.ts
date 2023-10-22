f15efd2d02adfcdfad6d4652dcb09b74a4b3e2c4
DavidFFDAW <davidferflo2@gmail.com>
2023-10-23 00:04:51 +0200

    fix: middleware fix incoming

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { NextRequestWithAuth, withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
-import { PROTECTED_ROUTES, STATIC_PAGES, USER_ROLES } from '@/constants/config';
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { PROTECTED_ROUTES, STATIC_PAGES, USER_ROLES } from "@/constants/config";
 
 export default withAuth(
     function middleware(request: NextRequestWithAuth) {
@@ -8,23 +8,37 @@ export default withAuth(
             role: string;
         };
 
-        const userRole = authRole?.role.toUpperCase();
+        console.log({ authRole });
+        console.log({ tk: request.nextauth.token });
 
-        if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.ADMIN) && userRole !== USER_ROLES.ADMIN) {
-            return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
+        const userRole = authRole?.role.toUpperCase();
+        console.log({ userRole });
+
+        if (
+            request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.ADMIN) &&
+            userRole !== USER_ROLES.ADMIN
+        ) {
+            return NextResponse.rewrite(
+                new URL(STATIC_PAGES.DENIED, request.url)
+            );
         }
 
-        if (request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.USER) && userRole !== USER_ROLES.USER) {
-            return NextResponse.rewrite(new URL(STATIC_PAGES.DENIED, request.url));
+        if (
+            request.nextUrl.pathname.startsWith(PROTECTED_ROUTES.USER) &&
+            userRole !== USER_ROLES.USER
+        ) {
+            return NextResponse.rewrite(
+                new URL(STATIC_PAGES.DENIED, request.url)
+            );
         }
     },
     {
         callbacks: {
             authorized: ({ token }) => !!token,
         },
-    },
+    }
 );
 
 export const config = {
-    matcher: ['/admin/:path*', '/user/:path'],
+    matcher: ["/admin/:path*", "/user/:path"],
 };
