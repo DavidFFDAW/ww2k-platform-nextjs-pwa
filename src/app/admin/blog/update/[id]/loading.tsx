db50a052c3b4b45793706ba32d050b866db60012
DavidFFDAW <davidferflo2@gmail.com>
2023-11-08 13:55:26 +0100

    feat: loading state for update page and styles

---
+++ src/app/admin/blog/update/[id]/loading.tsx
@@ -0,0 +1,10 @@
+import Spinner from '@/components/Spinner/Spinner'
+import React from 'react'
+
+export default function BlogUpdateLoadingLayout() {
+    return (
+        <>
+            <Spinner />
+        </>
+    )
+}
