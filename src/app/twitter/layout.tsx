fc8ddb63384e9eed696d9a55e372902df60f0542
DavidFFDAW <davidferflo2@gmail.com>
2023-11-08 11:02:14 +0100

    feat: twitter layout fix error

--- src/app/twitter/layout.tsx
+++ src/app/twitter/layout.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { LayoutProps } from '../../../.next/types/app/layout';
+import React from 'react';
+import { ChildrenInterface } from '@/shared/models';
 import './twitter.css';
 
-export default function TwitterPublicPageLayout({ children }: LayoutProps) {
+export default function TwitterPublicPageLayout({ children }: ChildrenInterface) {
     return (
         <div className='public-twitter-module'>
             <div className="twitter__module">
                 <div className="super-container">
                     <div className="cctt">
-
                         {children}
                     </div>
                 </div>
