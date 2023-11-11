900649e31afa63cf44130b2f4df0c45b28a618ad
DavidFFDAW <davidferflo2@gmail.com>
2023-11-11 14:32:27 +0100

    feat: teams exportation

--- src/app/admin/exportation/page.tsx
+++ src/app/admin/exportation/page.tsx
@@ -1,32 +1,55 @@
-import { Boxed } from '@/components/Box/Boxed'
-import Title from '@/components/Title'
-import Link from 'next/link'
-import React from 'react'
-import ExportLink from './ExportLink'
+import { Boxed } from "@/components/Box/Boxed";
+import Title from "@/components/Title";
+import Link from "next/link";
+import React from "react";
+import ExportLink from "./ExportLink";
 
 export default function AdminExportationPage() {
     return (
         <>
-            <Title title={'Exportacion'} icon="cloud-download" />
+            <Title title={"Exportacion"} icon="cloud-download" />
 
-            <Boxed w='100'>
+            <Boxed w="100">
+                <h4>
+                    El archivo que se descarga de la tabla es un archivo JSON
+                    con todos los datos de la tabla seleccionada
+                </h4>
 
-                <h4>El archivo que se descarga de la tabla es un archivo JSON con todos los datos de la tabla seleccionada</h4>
-
-                <div className='down'>
+                <div className="down">
                     <p>Selecciona la tabla para exportar:</p>
 
-                    <div className='down w1 flex column astart gap-small'>
-                        <ExportLink text='Wrestlers' endpoint='wrestlers' working={true} />
-                        <ExportLink text='Blog Posts' endpoint='blog' working={true} />
-                        <ExportLink text='Users' endpoint='users' working={true} />
-                        <ExportLink text='Equipos' endpoint='teams' />
-                        <ExportLink text='Campeonatos' endpoint='championships' />
-                        <ExportLink text='Reinados' endpoint='championship/reigns' />
+                    <div className="down w1 flex column astart gap-small">
+                        <ExportLink
+                            text="Wrestlers"
+                            endpoint="wrestlers"
+                            working={true}
+                        />
+                        <ExportLink
+                            text="Blog Posts"
+                            endpoint="blog"
+                            working={true}
+                        />
+                        <ExportLink
+                            text="Users"
+                            endpoint="users"
+                            working={true}
+                        />
+                        <ExportLink
+                            text="Equipos"
+                            endpoint="teams"
+                            working={true}
+                        />
+                        <ExportLink
+                            text="Campeonatos"
+                            endpoint="championships"
+                        />
+                        <ExportLink
+                            text="Reinados"
+                            endpoint="championship/reigns"
+                        />
                     </div>
                 </div>
             </Boxed>
-
         </>
-    )
+    );
 }
---
+++ src/app/api/exportation/teams/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/db/conn";
+import { NextRequest, NextResponse } from "next/server";
+import { getJWT, getNonTokenResponse } from "../../helpers/token.helper";
+
+export async function GET(request: NextRequest) {
+    const token = await getJWT(request);
+    if (!token) return getNonTokenResponse();
+
+    try {
+        const teams = await prisma.team.findMany({
+            include: {
+                actual_brand: true,
+                WrestlerTeam: true,
+            },
+        });
+        return NextResponse.json({ data: teams }, { status: 200 });
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
