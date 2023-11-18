351763be3f56b90246e472d11db081ab18efe24c
DavidFFDAW <davidferflo2@gmail.com>
2023-11-18 18:39:21 +0100

    refactor: added className to Boxed component

--- src/components/Box/Boxed.tsx
+++ src/components/Box/Boxed.tsx
@@ -4,12 +4,16 @@ interface BoxedProps {
     title?: string;
     children: React.ReactNode;
     w?: string;
+    className?: string;
 }
 
-export function Boxed({ title, children, w }: BoxedProps) {
+export function Boxed({ title, children, w, className }: BoxedProps) {
     const width = w ? Number(w) : 90;
+    const classes = className
+        ? `boxed space-down ${className}`
+        : "boxed space-down";
     return (
-        <div className="boxed space-down" style={{ width: `${width}%` }}>
+        <div className={classes} style={{ width: `${width}%` }}>
             {title && <h2 className="box-title space-down">{title}</h2>}
             {children}
         </div>
