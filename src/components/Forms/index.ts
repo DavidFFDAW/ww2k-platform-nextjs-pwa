866971a47cb56cdd778d4324564f92df537f6fd9
DavidFFDAW <davidferflo2@gmail.com>
2023-11-19 23:26:55 +0100

    feat: updated Forms components barrel

--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -1,4 +1,8 @@
-export * from './Inputs';
-export { default as QuillEditor } from './QuillEditor/QuillEditor';
-export { default as SearchForm } from './SearchForm';
-export * from './index';
+export { default as Form } from "./Form/Form";
+export { default as useForm } from "./Form/useForm";
+export { default as HttpForm } from "./HttpForm/HttpForm";
+export { default as useHttpForm } from "./HttpForm/useHttpForm";
+export * from "./Inputs";
+export { default as QuillEditor } from "./QuillEditor/QuillEditor";
+export * from "./QuillEditor/quill.editor.model";
+export { default as SearchForm } from "./SearchForm";
