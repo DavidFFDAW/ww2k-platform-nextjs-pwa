491689be7d085a2069c0464c284ba1723a1522e9
DavidFFDAW <davidferflo2@gmail.com>
2023-11-29 10:32:14 +0100

    feat: draft listing and random choosing

---
+++ src/app/admin/draft/DraftPick.tsx
@@ -0,0 +1,70 @@
+import { NullableLoading } from '@/components/Loading';
+import { Wrestler } from '@prisma/client';
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+    wrestler: Wrestler;
+    brand: 'RAW' | 'SMACKDOWN';
+}
+
+interface StateInterface {
+    className: string;
+    show: boolean;
+}
+
+export default function DraftPick({ wrestler, brand }: Props) {
+
+    const [draftPickState, setDraftPickState] = useState<StateInterface>({
+        className: 'anim-appear',
+        show: true
+    });
+
+    const handleAnimationEnd = () => {
+        if (draftPickState.className === 'anim-appear') {
+            setTimeout(() => {
+                setDraftPickState(prev => ({
+                    ...prev,
+                    className: 'anim-disappear',
+                }));
+            }, 1200);
+        }
+
+        if (draftPickState.className === 'anim-disappear') {
+            setTimeout(() => {
+                setDraftPickState(prev => ({
+                    ...prev,
+                    show: false,
+                }));
+            }, 500);
+        }
+
+        console.log('Animation ended');
+    }
+
+    const brands: any = {
+        RAW: '/raw-roster.jpg',
+        SMACKDOWN: '/sd-roster.jpg',
+    };
+    const brandImage: string = brands[brand.toUpperCase()] || brands.RAW;
+
+    return (
+        <NullableLoading condition={draftPickState.show}>
+            <div
+                className={`${draftPickState.className}  draft wrestler-card`}
+                style={{ backgroundImage: `url(${brandImage})` }}
+                onAnimationEnd={handleAnimationEnd}>
+                <div className="draft-wrestler-card-name">
+                    <span>{wrestler.name}</span>
+                </div>
+                <div className="draft-wrestler-image">
+                    <img draggable={false} src={wrestler.image_name as string} alt={wrestler.name} />
+                </div>
+                {/* {wrestler.championship && (
+                <div className="draft-championship-image">
+                    <img draggable={false} src={wrestler.championship.image} alt={wrestler.championship.name} />
+                </div>
+            )} */}
+            </div>
+        </NullableLoading>
+    )
+}
--- src/app/admin/draft/useDraft.ts
+++ src/app/admin/draft/useDraft.ts
@@ -4,12 +4,16 @@ import { useState } from 'react'
 interface DraftStore {
     draftWrestlers: Wrestler[];
     userSelection: Wrestler[];
+    raw: Wrestler[];
+    smackdown: Wrestler[];
 }
 
 export default function useDraft(wrestlers: Wrestler[]) {
     const [state, setState] = useState<DraftStore>({
         draftWrestlers: wrestlers,
         userSelection: [],
+        raw: [],
+        smackdown: [],
     });
 
     const handleFormSubmition = (datas: any) => {
@@ -18,19 +22,40 @@ export default function useDraft(wrestlers: Wrestler[]) {
         const foundInSelection = state.userSelection.find((wrestler) => wrestler.id === selectedID);
 
         if (!selectedWrestler || foundInSelection) return;
+        const listAfterSelection = state.draftWrestlers.filter((wrestler) => wrestler.id !== selectedID);
+        const rawBrandSelection = chooseRawWrestler(selectedWrestler, listAfterSelection);
 
         setState((prev: DraftStore) => {
             return {
                 ...prev,
                 draftWrestlers: prev.draftWrestlers.filter((wrestler: Wrestler) => wrestler.id !== selectedID),
-                userSelection: [...prev.userSelection, selectedWrestler],
+                // userSelection: [...prev.userSelection, selectedWrestler],
+                raw: rawBrandSelection ? [...prev.raw, rawBrandSelection] : [...prev.raw],
+                smackdown: [...prev.smackdown, selectedWrestler],
             };
         });
     }
 
+    const chooseRawWrestler = (smackdownPick: Wrestler, listAfterSmackdownPick: Wrestler[]) => {
+        if (listAfterSmackdownPick.length <= 0) {
+            return false;
+        }
+        const genderList = listAfterSmackdownPick.filter((it) => it.sex === smackdownPick.sex);
+        const finalGender = genderList.length <= 0 ? listAfterSmackdownPick : genderList;
+        const overallList = finalGender.filter((it) => it.overall >= smackdownPick.overall - 5 && it.overall <= smackdownPick.overall + 5);
+        const finalList = overallList.length <= 0 ? finalGender : overallList;
+
+        const random = Math.floor(Math.random() * finalList.length);
+        const selectedWrestler = finalList[random];
+
+        return selectedWrestler;
+    };
+
     return {
         draftWrestlers: state.draftWrestlers,
         userSelection: state.userSelection,
+        smackdown: state.smackdown,
+        raw: state.raw,
         handleFormSubmition,
     }
 }
