15f9f62f4e2ac791f4d015fcfa31a19e41014ec3
carloslopez2023 <clopez@crownhillshoes.com>
2023-11-30 19:05:35 +0100

    feat: Draft page related stuff

--- src/app/admin/draft/useDraft.ts
+++ src/app/admin/draft/useDraft.ts
@@ -1,11 +1,12 @@
-import { Wrestler } from '@prisma/client';
-import { useState } from 'react'
+import { Wrestler } from "@prisma/client";
+import { useState } from "react";
 
 interface DraftStore {
     draftWrestlers: Wrestler[];
     userSelection: Wrestler[];
     raw: Wrestler[];
     smackdown: Wrestler[];
+    removeSelectText: boolean;
 }
 
 export default function useDraft(wrestlers: Wrestler[]) {
@@ -14,35 +15,59 @@ export default function useDraft(wrestlers: Wrestler[]) {
         userSelection: [],
         raw: [],
         smackdown: [],
+        removeSelectText: false,
     });
 
     const handleFormSubmition = (datas: any) => {
         const selectedID = Number(datas.selected_wrestler);
-        const selectedWrestler = wrestlers.find((wrestler) => wrestler.id === selectedID);
-        const foundInSelection = state.userSelection.find((wrestler) => wrestler.id === selectedID);
+        const selectedWrestler = wrestlers.find(
+            (wrestler) => wrestler.id === selectedID
+        );
+        const foundInSelection = state.userSelection.find(
+            (wrestler) => wrestler.id === selectedID
+        );
 
         if (!selectedWrestler || foundInSelection) return;
-        const listAfterSelection = state.draftWrestlers.filter((wrestler) => wrestler.id !== selectedID);
-        const rawBrandSelection = chooseRawWrestler(selectedWrestler, listAfterSelection);
+        const listAfterSelection = state.draftWrestlers.filter(
+            (wrestler) => wrestler.id !== selectedID
+        );
+        const rawBrandSelection = chooseRawWrestler(
+            selectedWrestler,
+            listAfterSelection
+        );
 
         setState((prev: DraftStore) => {
             return {
                 ...prev,
-                draftWrestlers: prev.draftWrestlers.filter((wrestler: Wrestler) => wrestler.id !== selectedID),
+                draftWrestlers: prev.draftWrestlers.filter(
+                    (wrestler: Wrestler) => wrestler.id !== selectedID
+                ),
                 // userSelection: [...prev.userSelection, selectedWrestler],
-                raw: rawBrandSelection ? [...prev.raw, rawBrandSelection] : [...prev.raw],
+                raw: rawBrandSelection
+                    ? [...prev.raw, rawBrandSelection]
+                    : [...prev.raw],
                 smackdown: [...prev.smackdown, selectedWrestler],
             };
         });
-    }
+    };
 
-    const chooseRawWrestler = (smackdownPick: Wrestler, listAfterSmackdownPick: Wrestler[]) => {
+    const chooseRawWrestler = (
+        smackdownPick: Wrestler,
+        listAfterSmackdownPick: Wrestler[]
+    ) => {
         if (listAfterSmackdownPick.length <= 0) {
             return false;
         }
-        const genderList = listAfterSmackdownPick.filter((it) => it.sex === smackdownPick.sex);
-        const finalGender = genderList.length <= 0 ? listAfterSmackdownPick : genderList;
-        const overallList = finalGender.filter((it) => it.overall >= smackdownPick.overall - 5 && it.overall <= smackdownPick.overall + 5);
+        const genderList = listAfterSmackdownPick.filter(
+            (it) => it.sex === smackdownPick.sex
+        );
+        const finalGender =
+            genderList.length <= 0 ? listAfterSmackdownPick : genderList;
+        const overallList = finalGender.filter(
+            (it) =>
+                it.overall >= smackdownPick.overall - 5 &&
+                it.overall <= smackdownPick.overall + 5
+        );
         const finalList = overallList.length <= 0 ? finalGender : overallList;
 
         const random = Math.floor(Math.random() * finalList.length);
@@ -56,6 +81,7 @@ export default function useDraft(wrestlers: Wrestler[]) {
         userSelection: state.userSelection,
         smackdown: state.smackdown,
         raw: state.raw,
+        removeSelectText: state.removeSelectText,
         handleFormSubmition,
-    }
+    };
 }
