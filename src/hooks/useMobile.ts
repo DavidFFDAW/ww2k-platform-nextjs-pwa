1a026451c1fd7f33f14e768edd0543d9b1dcff72
DavidFFDAW <davidferflo2@gmail.com>
2023-11-30 18:25:37 +0100

    feat: useMobile global hook

---
+++ src/hooks/useMobile.ts
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+export default function useMobile() {
+    const [width, setWidth] = React.useState(window.innerWidth);
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowSizeChange);
+        return () => {
+            window.removeEventListener("resize", handleWindowSizeChange);
+        };
+    }, []);
+
+    return {
+        isMobile: width <= 768,
+    };
+}
