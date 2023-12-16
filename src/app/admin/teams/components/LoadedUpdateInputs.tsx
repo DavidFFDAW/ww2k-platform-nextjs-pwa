3ef374750d68b69421a0686151214fc5961dac47
DavidFFDAW <davidferflo2@gmail.com>
2023-12-16 18:03:34 +0100

    feat: admin teams page does not have a form anymore

--- src/app/admin/teams/components/LoadedUpdateInputs.tsx
+++ src/app/admin/teams/components/LoadedUpdateInputs.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import { getCompleteTeamDataByID } from "@/queries/teams.queries";
 import { HiddenInput } from "@/components/Forms/Inputs/Hidden";
 import { Boxed } from "@/components/Box/Boxed";
-import { Input, ToggleInput } from "@/components/Forms";
+import { BrandSelect, Form, Input, ToggleInput } from "@/components/Forms";
 import { NumberInput } from "@/components/Forms/Inputs/Number";
 import GroupSelection from "./GroupSelection";
 import { getWrestlersWithoutTeam } from "@/queries/wrestler.queries";
-
-function getCurrentTeamData(id: string) {
-    return getCompleteTeamDataByID(id);
-}
+import Title from "@/components/Title";
+import { ButtonCTA } from "@/components/Buttons/Buttons";
 
 export default async function LoadedUpdateInputs({ id }: { id: string }) {
     const [currentTeamData, possibleMembers] = await Promise.all([
-        getCurrentTeamData(id),
+        getCompleteTeamDataByID(id),
         getWrestlersWithoutTeam(),
     ]);
     if (!currentTeamData || !possibleMembers || !id) {
@@ -28,33 +26,62 @@ export default async function LoadedUpdateInputs({ id }: { id: string }) {
 
     return (
         <>
-            <Boxed title={"Datos del equipo"} w={"100"}>
-                <div className="flex center aend gap-small">
-                    <HiddenInput name={"id"} value={currentTeamData.id} />
-                    <Input
-                        required={true}
-                        max={150}
-                        label={"Nombre"}
-                        name={"name"}
-                        value={currentTeamData.name}
-                    />
-                    <NumberInput
-                        type={"number"}
-                        max={3}
-                        required={false}
-                        label={"Media"}
-                        name={"overall"}
-                        value={currentTeamData.average}
-                    />
-                    <ToggleInput
-                        label={"Activo"}
-                        name={"active"}
-                        checked={currentTeamData.active}
-                    />
-                </div>
-            </Boxed>
+            <Title title={`Actualizar ${currentTeamData.name}`} />
 
-            <GroupSelection list={possibleMembers} members={teamMembers} />
+            <Form
+                method="PUT"
+                action={`/api/teams/update/${currentTeamData.id}`}
+                className="flex center al-center column gap wrestler-upsert-form space-down"
+                sendHttp={true}
+                refresh={true}
+            >
+                <Boxed title={"Datos del equipo"} w={"100"}>
+                    <div className="grid two-column-grid aend gap-small">
+                        <HiddenInput name={"id"} value={currentTeamData.id} />
+                        <Input
+                            required={true}
+                            max={150}
+                            label={"Nombre"}
+                            name={"name"}
+                            value={currentTeamData.name}
+                        />
+                        <NumberInput
+                            type={"number"}
+                            max={3}
+                            required={false}
+                            label={"Media"}
+                            name={"overall"}
+                            value={currentTeamData.average}
+                        />
+                        <Input
+                            required={true}
+                            max={150}
+                            label={"Slug"}
+                            name={"slug"}
+                            value={currentTeamData.slug || currentTeamData.name}
+                        />
+                        <ToggleInput
+                            label={"Activo"}
+                            name={"active"}
+                            checked={currentTeamData.active}
+                        />
+                    </div>
+                </Boxed>
+
+                <GroupSelection list={possibleMembers} members={teamMembers} />
+
+                <Boxed title={"Datos de la marca"} w={"100"}>
+                    <BrandSelect
+                        label={"Marca"}
+                        name={"brand"}
+                        value={currentTeamData.brand}
+                    />
+                </Boxed>
+
+                <div className="flex end acenter fixed-button">
+                    <ButtonCTA type={"submit"} text={"Guardar"} />
+                </div>
+            </Form>
         </>
     );
 }
--- src/app/admin/teams/update/[id]/page.tsx
+++ src/app/admin/teams/update/[id]/page.tsx
@@ -1,21 +1,14 @@
-import Form from "@/components/Forms/Form/Form";
 import React, { Suspense } from "react";
-import { ButtonCTA } from "@/components/Buttons/Buttons";
-import { getTeamDataByID } from "@/queries/teams.queries";
 import TeamUpdateSkeleton from "../../components/TeamUpdateSkeleton";
 import LoadedUpdateInputs from "../../components/LoadedUpdateInputs";
+import { Metadata } from "next";
 
 export const revalidate = 0;
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-    const team = await getTeamDataByID(params.id);
-    const title = !team ? "Equipo no encontrado" : `Actualizar ${team.name}`;
-
-    return {
-        title: title,
-        description: "Actualizar equipo",
-    };
-}
+export const metadata: Metadata = {
+    title: "Actualizar equipo",
+    description: "Actualizar equipo",
+};
 
 export default function AdminTeamUpdatePage({
     params,
@@ -26,23 +19,9 @@ export default function AdminTeamUpdatePage({
 
     return (
         <>
-            <Form
-                method="PUT"
-                action={`/api/teams/update/${id}`}
-                className="flex center al-center column gap wrestler-upsert-form space-down"
-                debug={true}
-                sendHttp={true}
-                redirect="/admin/teams"
-                refresh={true}
-            >
-                <Suspense fallback={<TeamUpdateSkeleton />}>
-                    <LoadedUpdateInputs id={id} />
-                </Suspense>
-
-                <div className="flex end acenter fixed-button">
-                    <ButtonCTA type={"submit"} text={"Guardar"} />
-                </div>
-            </Form>
+            <Suspense fallback={<TeamUpdateSkeleton />}>
+                <LoadedUpdateInputs id={id} />
+            </Suspense>
         </>
     );
 }
