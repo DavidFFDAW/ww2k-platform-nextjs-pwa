a37a44e3d2567ccb605181e3e2fa0c591d9b3645
DavidFFDAW <davidferflo2@gmail.com>
2024-02-17 15:09:04 +0100

    feat: is_adjustable field

--- src/app/api/championships/reigns/dates_adjust/route.ts
+++ src/app/api/championships/reigns/dates_adjust/route.ts
@@ -17,6 +17,9 @@ export async function POST() {
                 Wrestler: true,
                 Team: true,
             },
+            where: {
+                is_adjustable: true,
+            },
         });
 
         const group: any = {};
@@ -27,46 +30,55 @@ export async function POST() {
             group[reign.Championship.id].push(reign);
         });
 
-        const updatingChampionshipReigns = Object.entries(group).reduce((acc: any, [_, value]: [string, any]) => {
-            value.forEach((reign: any, index: number) => {
-                const nextReign = value[index + 1];
-                if (!nextReign) {
-                    return;
-                }
+        const updatingChampionshipReigns = Object.entries(group).reduce(
+            (acc: any, [_, value]: [string, any]) => {
+                value.forEach((reign: any, index: number) => {
+                    const nextReign = value[index + 1];
+                    if (!nextReign) {
+                        return;
+                    }
 
-                const currentWinDate = new Date(reign.won_date);
-                const nextReignDate = new Date(nextReign?.won_date);
+                    const currentWinDate = new Date(reign.won_date);
+                    const nextReignDate = new Date(nextReign?.won_date);
 
-                const daysBetween = Math.floor(
-                    (nextReignDate.getTime() - currentWinDate.getTime()) /
-                    (1000 * 60 * 60 * 24)
-                );
+                    const daysBetween = Math.floor(
+                        (nextReignDate.getTime() - currentWinDate.getTime()) /
+                            (1000 * 60 * 60 * 24)
+                    );
 
-                acc = [...acc, {
-                    ...reign,
-                    title: reign.Championship.name,
-                    man: reign.Wrestler?.name,
-                    days: daysBetween,
-                    lost_date: nextReign?.won_date,
-                }];
-            });
-            return acc;
-        }, []);
+                    acc = [
+                        ...acc,
+                        {
+                            ...reign,
+                            title: reign.Championship.name,
+                            man: reign.Wrestler?.name,
+                            days: daysBetween,
+                            lost_date: nextReign?.won_date,
+                        },
+                    ];
+                });
+                return acc;
+            },
+            []
+        );
 
-        const affectedRows = updatingChampionshipReigns.reduce(async (accumulator: number, element: any) => {
-            const result = await prisma.championshipReign.update({
-                where: {
-                    id: element.id,
-                },
-                data: {
-                    days: element.days,
-                    lost_date: element.lost_date,
-                },
-            });
+        const affectedRows = updatingChampionshipReigns.reduce(
+            async (accumulator: number, element: any) => {
+                const result = await prisma.championshipReign.update({
+                    where: {
+                        id: element.id,
+                    },
+                    data: {
+                        days: element.days,
+                        lost_date: element.lost_date,
+                    },
+                });
 
-            if (result.id) accumulator++;
-            return accumulator;
-        }, 0);
+                if (result.id) accumulator++;
+                return accumulator;
+            },
+            0
+        );
 
         return NextResponse.json(
             {
@@ -76,7 +88,7 @@ export async function POST() {
                 status: 200,
             }
         );
-    } catch (error: any) { 
+    } catch (error: any) {
         return NextResponse.json(
             {
                 message: error.message,
