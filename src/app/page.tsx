6ed76f0ddb947f3c0a7afdca985f91a74aa78b4d
DavidFFDAW <davidferflo2@gmail.com>
2024-03-16 18:36:38 +0100

    feat: home links changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,50 @@
-import PageBackground from '@/components/PageBackground/PageBackground';
-import HomeLink from '@/components/HomeLink/HomeLink';
-import { PublicMenu } from '@/constants/routes';
-import { BootstrapIcon } from '@/components/Icon/BootstrapIcon';
+import PageBackground from "@/components/PageBackground/PageBackground";
+import HomeLink from "@/components/HomeLink/HomeLink";
+import { PublicMenu } from "@/constants/routes";
+import { BootstrapIcon } from "@/components/Icon/BootstrapIcon";
+import BodyDeletableClass from "@/components/PageBackground/BodyDeletableClass";
 
 function Home() {
     return (
-        <PageBackground custom={'principal'}>
+        <>
+            <BodyDeletableClass className="custom-totalpage-body-wallpaper custom-wallpaper principal" />
             <div className="dashboard-home flex end column acenter">
                 <div className="home-spas-links">
-                    {PublicMenu.map((item, index) => {
-                        return <HomeLink href={item.url} icon={<item.icon />} text={item.name} key={index} />;
-                    })}
-                    <HomeLink href={'/roster'} text={'Roster'} icon={<BootstrapIcon icon={'list-ul'} />} />
+                    {/* {PublicMenu.map((item, index) => {
+                        return (
+                            <HomeLink
+                                href={item.url}
+                                icon={<item.icon />}
+                                text={item.name}
+                                key={index}
+                            />
+                        );
+                    })} */}
+                    <HomeLink
+                        href={"/roster"}
+                        text={"Roster"}
+                        icon={<BootstrapIcon icon={"list-ul"} />}
+                    />
+                    <HomeLink
+                        text={"Admin"}
+                        href={"/admin"}
+                        className="admin"
+                        icon={<BootstrapIcon icon={"lock-fill"} />}
+                    />
+                    <HomeLink
+                        href={"/blog"}
+                        text={"Blog"}
+                        icon={<BootstrapIcon icon={"newspaper"} />}
+                    />
+                    <HomeLink
+                        href={"/twitter"}
+                        text={"Twitter"}
+                        className="twitter"
+                        icon={<BootstrapIcon icon={"twitter-x"} />}
+                    />
                 </div>
             </div>
-        </PageBackground>
+        </>
     );
 }
 
--- src/components/HomeLink/HomeLink.tsx
+++ src/components/HomeLink/HomeLink.tsx
@@ -1,15 +1,18 @@
-import Link from 'next/link';
-import { NullableLoading } from '../Loading';
+import Link from "next/link";
+import { NullableLoading } from "../Loading";
 
 interface Props {
     icon?: React.ReactNode;
     href: string;
     text: string;
+    className?: string;
 }
 
-export default function HomeLink({ icon, href, text }: Props) {
+export default function HomeLink({ icon, href, text, className }: Props) {
+    const classname = ["home-links", className].join(" ").trim();
+
     return (
-        <Link className="home-links" href={href}>
+        <Link className={classname} href={href}>
             <NullableLoading condition={Boolean(icon)}>{icon}</NullableLoading>
             <div className="block">
                 <span className="link">{text}</span>
