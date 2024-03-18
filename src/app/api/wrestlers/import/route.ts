c193361bfa833544a731aefaa31c3cc151c11dad
DavidFFDAW <davidferflo2@gmail.com>
2024-03-18 01:18:21 +0100

    feat: importation csv wrestlers done

--- src/app/api/wrestlers/import/route.ts
+++ src/app/api/wrestlers/import/route.ts
@@ -1,7 +1,7 @@
-import { prisma } from '@/db/conn';
-import { Wrestler } from '@prisma/client';
-import { NextResponse } from 'next/server';
-import { getAllWrestlers } from '@/queries/wrestler.queries';
+import { prisma } from "@/db/conn";
+import { Wrestler } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { getAllWrestlers } from "@/queries/wrestler.queries";
 
 const hasWrestlerChanged = (wrestler: Wrestler, updateWrestlerData: any) => {
     return (
@@ -21,41 +21,52 @@ const hasWrestlerChanged = (wrestler: Wrestler, updateWrestlerData: any) => {
 function getUpdateAndInsertWrestlers(dataJson: any[]) {
     return {
         update: dataJson.filter((wrestler: any) => wrestler.id),
-        insert: dataJson.filter((wrestler: any) => (!wrestler.id || wrestler.id == 0) && wrestler.name),
+        insert: dataJson.filter(
+            (wrestler: any) =>
+                (!wrestler.id || wrestler.id == 0) && wrestler.name
+        ),
     };
 }
 
 export async function POST(req: Request) {
-    const body = await req.json();
-    const wrestlers = await getAllWrestlers();
-    const dataJson = body._importcsv;
-
-    if (!dataJson) {
-        return NextResponse.json({ message: 'No se ha enviado ningun archivo' }, { status: 400 });
-    }
-
-    const parsed = JSON.parse(dataJson);
-    if (!parsed) {
-        return NextResponse.json({ message: 'No se ha podido parsear el archivo' }, { status: 400 });
-    }
-
-    const parsedDatas = parsed.map((value: any) => {
-        return {
-            ...value,
-            id: Number(value.id),
-            overall: Number(value.overall),
-        };
-    });
-
-    const { update, insert } = getUpdateAndInsertWrestlers(parsedDatas);
-
-    const changedWrestlers = update.filter((updateWrestlerData: any) => {
-        const wrestler = wrestlers.find((w: Wrestler) => w.id === Number(updateWrestlerData.id));
-        if (!wrestler) return false;
-        return hasWrestlerChanged(wrestler, updateWrestlerData);
-    });
-
     try {
+        const body = await req.json();
+        const wrestlers = await getAllWrestlers();
+        const dataJson = body._importcsv;
+
+        if (!dataJson) {
+            return NextResponse.json(
+                { message: "No se ha enviado ningun archivo" },
+                { status: 400 }
+            );
+        }
+
+        const parsed = JSON.parse(dataJson);
+        if (!parsed) {
+            return NextResponse.json(
+                { message: "No se ha podido parsear el archivo" },
+                { status: 400 }
+            );
+        }
+
+        const parsedDatas = parsed.map((value: any) => {
+            return {
+                ...value,
+                id: Number(value.id),
+                overall: Number(value.overall),
+            };
+        });
+
+        const { update, insert } = getUpdateAndInsertWrestlers(parsedDatas);
+
+        const changedWrestlers = update.filter((updateWrestlerData: any) => {
+            const wrestler = wrestlers.find(
+                (w: Wrestler) => w.id === Number(updateWrestlerData.id)
+            );
+            if (!wrestler) return false;
+            return hasWrestlerChanged(wrestler, updateWrestlerData);
+        });
+
         for (const updatingWrestler of changedWrestlers) {
             await prisma.wrestler.update({
                 where: { id: updatingWrestler.id },
@@ -72,9 +83,34 @@ export async function POST(req: Request) {
                 },
             });
         }
-    } catch (err) {
-        return NextResponse.json({ message: 'Error al actualizar los luchadores' }, { status: 500 });
-    }
 
-    return NextResponse.json({ message: `Se han actualizado ${changedWrestlers.length} luchadores` }, { status: 200 });
+        for (const newWrestler of insert) {
+            await prisma.wrestler.create({
+                data: {
+                    name: newWrestler.name,
+                    alias: newWrestler.alias,
+                    brand: newWrestler.brand,
+                    status: newWrestler.status,
+                    overall: newWrestler.overall,
+                    finisher: newWrestler.finisher,
+                    twitter_acc: newWrestler.twitter_account,
+                    twitter_name: newWrestler.twitter_name,
+                    kayfabe_status: newWrestler.kayfabe,
+                    sex: newWrestler.sex,
+                },
+            });
+        }
+
+        return NextResponse.json(
+            {
+                message: `Se han actualizado ${changedWrestlers.length} luchadores y se han insertado ${insert.length} luchadores`,
+            },
+            { status: 200 }
+        );
+    } catch (err) {
+        return NextResponse.json(
+            { message: "Error al actualizar los luchadores" },
+            { status: 500 }
+        );
+    }
 }
