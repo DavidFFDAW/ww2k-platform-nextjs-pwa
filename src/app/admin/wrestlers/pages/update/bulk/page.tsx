7f986c6e3053dd69c78e42e9d30640f71a4a7296
DavidFFDAW <davidferflo2@gmail.com>
2024-03-20 00:36:29 +0100

    feat: wrestlers bulk update table

--- src/app/admin/wrestlers/pages/update/bulk/page.tsx
+++ src/app/admin/wrestlers/pages/update/bulk/page.tsx
@@ -1,11 +1,14 @@
-import { Form } from '@/components/Forms';
-import { NullableLoading } from '@/components/Loading';
-import { Pagination } from '@/components/Pagination';
-import CustomTable from '@/components/Table/CustomTable';
-import { getCountedWrestlers, getWrestlersByLimit } from '@/queries/wrestler.queries';
-import { PageContext } from '@/shared/models';
-import { Wrestler } from '@prisma/client';
-import React from 'react';
+import { Form } from "@/components/Forms";
+import { NullableLoading } from "@/components/Loading";
+import { Pagination } from "@/components/Pagination";
+import CustomTable from "@/components/Table/CustomTable";
+import {
+    getCountedWrestlers,
+    getWrestlersByLimit,
+} from "@/queries/wrestler.queries";
+import { PageContext } from "@/shared/models";
+import { Wrestler } from "@prisma/client";
+import React from "react";
 
 export const revalidate = 0;
 
@@ -18,16 +21,32 @@ export default async function WrestlersBulkUpdatePage(context: PageContext) {
     const wrestlersLength = await getCountedWrestlers();
 
     return (
-        <Form method="POST" action="/api/wrestlers/update/bulk" sendHttp={true} refresh={false} loadingState={true}>
+        <Form
+            method="POST"
+            action="/api/wrestlers/update/bulk"
+            sendHttp={true}
+            refresh={true}
+            loadingState={true}
+        >
             <div className="w1 flex end acenter">
-                <Pagination page={Number(page)} total={wrestlersLength} numberOfPages={2} itemsPerPage={limit} />
+                <Pagination
+                    page={Number(page)}
+                    total={wrestlersLength}
+                    numberOfPages={2}
+                    itemsPerPage={limit}
+                />
             </div>
+
+            <input type="hidden" name="limit" value={limit} />
+            <input type="hidden" name="offset" value={offset} />
 
             <CustomTable title="Luchadores">
                 <table>
                     <thead>
                         <tr>
-                            <th colSpan={4}>Actualmente mostrando: {wrestlers.length}</th>
+                            <th colSpan={4}>
+                                Actualmente mostrando: {wrestlers.length}
+                            </th>
                             <th colSpan={2}>
                                 {/* De {wrestlers[0].name[0]} - {wrestlers[wrestlers.length - 1].name[0]} */}
                             </th>
@@ -47,22 +66,90 @@ export default async function WrestlersBulkUpdatePage(context: PageContext) {
                         <NullableLoading condition={wrestlers.length > 1}>
                             {wrestlers.map((item: Wrestler) => {
                                 return (
-                                    <tr key={item.id} className="inputs-row-no-input-appearance">
+                                    <tr
+                                        key={item.id}
+                                        className="inputs-row-no-input-appearance"
+                                    >
                                         <td>
                                             {item.id}
-                                            <input type="hidden" name="id[]" defaultValue={item.id.toString()} />
+                                            <input
+                                                type="hidden"
+                                                name="id[]"
+                                                defaultValue={item.id.toString()}
+                                            />
                                         </td>
                                         <td>
-                                            <input type="text" name="name[]" defaultValue={item.name} />
+                                            <input
+                                                type="text"
+                                                name="name[]"
+                                                defaultValue={item.name}
+                                            />
                                         </td>
                                         <td>
-                                            <input type="text" name="alias[]" defaultValue={item.alias?.toString()} />
+                                            <input
+                                                type="text"
+                                                name="alias[]"
+                                                defaultValue={item.alias?.toString()}
+                                            />
                                         </td>
                                         <td>
-                                            <select name="status[]" defaultValue={item.status}>
-                                                <option value="active">Activo</option>
-                                                <option value="inactive">Inactivo</option>
-                                                <option value="released">Despedido</option>
+                                            <select
+                                                name="status[]"
+                                                defaultValue={item.status}
+                                            >
+                                                <option
+                                                    value="active"
+                                                    selected={
+                                                        item.status === "active"
+                                                    }
+                                                >
+                                                    Activo
+                                                </option>
+                                                <option
+                                                    value="not-active"
+                                                    selected={
+                                                        item.status ===
+                                                        "not-active"
+                                                    }
+                                                >
+                                                    Inactivo
+                                                </option>
+                                                <option
+                                                    value="released"
+                                                    selected={
+                                                        item.status ===
+                                                        "released"
+                                                    }
+                                                >
+                                                    Despedido
+                                                </option>
+                                                <option
+                                                    value="semi-active"
+                                                    selected={
+                                                        item.status ===
+                                                        "semi-active"
+                                                    }
+                                                >
+                                                    Part-time
+                                                </option>
+                                                <option
+                                                    value="retired"
+                                                    selected={
+                                                        item.status ===
+                                                        "retired"
+                                                    }
+                                                >
+                                                    Retirado
+                                                </option>
+                                                <option
+                                                    value="manager"
+                                                    selected={
+                                                        item.status ===
+                                                        "manager"
+                                                    }
+                                                >
+                                                    Manager
+                                                </option>
                                             </select>
                                         </td>
                                         <td>
@@ -72,7 +159,39 @@ export default async function WrestlersBulkUpdatePage(context: PageContext) {
                                                 defaultValue={item.overall.toString()}
                                             />
                                         </td>
-                                        <td>{item.brand}</td>
+                                        <td>
+                                            <select
+                                                name="brand[]"
+                                                defaultValue={item.brand}
+                                            >
+                                                <option
+                                                    value="RAW"
+                                                    selected={
+                                                        item.brand === "RAW"
+                                                    }
+                                                >
+                                                    RAW
+                                                </option>
+
+                                                <option
+                                                    value="AWL"
+                                                    selected={
+                                                        item.brand === "AWL"
+                                                    }
+                                                >
+                                                    AWL
+                                                </option>
+
+                                                <option
+                                                    value="SD"
+                                                    selected={
+                                                        item.brand === "SD"
+                                                    }
+                                                >
+                                                    SmackDown Live
+                                                </option>
+                                            </select>
+                                        </td>
                                     </tr>
                                 );
                             })}
@@ -82,7 +201,12 @@ export default async function WrestlersBulkUpdatePage(context: PageContext) {
             </CustomTable>
 
             <div className="fixed-button">
-                <button className="btn button cta" role="button" type="submit" title="Call to action button">
+                <button
+                    className="btn button cta"
+                    role="button"
+                    type="submit"
+                    title="Call to action button"
+                >
                     Actualizar
                 </button>
             </div>
--- src/app/api/wrestlers/update/bulk/route.ts
+++ src/app/api/wrestlers/update/bulk/route.ts
@@ -1,27 +1,75 @@
-import { NextResponse } from 'next/server';
+import { prisma } from "@/db/conn";
+import { Wrestler } from "@prisma/client";
+import { NextResponse } from "next/server";
+import { getWrestlersByLimit } from "@/queries/wrestler.queries";
+
+const hasWrestlerChanged = (wrestler: Wrestler, updateWrestlerData: any) => {
+    return (
+        updateWrestlerData.name !== wrestler.name ||
+        updateWrestlerData.alias !== wrestler.alias ||
+        updateWrestlerData.brand !== wrestler.brand ||
+        updateWrestlerData.status !== wrestler.status ||
+        updateWrestlerData.overall !== wrestler.overall
+    );
+};
 
 export async function POST(req: Request) {
     const body = await req.json();
+    const limit = Number(body.limit) || 20;
+    const offset = Number(body.offset) || 0;
+    const wrestlers = await getWrestlersByLimit(limit, offset);
 
-    const object = body.id.map((id: string, index: number) => {
+    const arrayWrestlers = body.id.map((id: string, index: number) => {
         return {
             id: Number(id),
             name: body.name[index],
             alias: body.alias[index],
-            overall: body.overall[index],
+            overall: Number(body.overall[index]),
+            status: body.status[index],
+            brand: body.brand[index],
         };
     });
 
-    console.log({
-        object,
-    });
+    const changedWrestlers = arrayWrestlers.filter(
+        (updateWrestlerData: any) => {
+            const wrestler = wrestlers.find(
+                (w: Wrestler) => w.id === Number(updateWrestlerData.id)
+            );
+            if (!wrestler) return false;
+            return hasWrestlerChanged(wrestler, updateWrestlerData);
+        }
+    );
+
+    try {
+        for (const updatingWrestler of changedWrestlers) {
+            await prisma.wrestler.update({
+                where: { id: updatingWrestler.id },
+                data: {
+                    name: updatingWrestler.name,
+                    alias: updatingWrestler.alias,
+                    brand: updatingWrestler.brand,
+                    status: updatingWrestler.status,
+                    overall: updatingWrestler.overall,
+                },
+            });
+        }
+    } catch (error) {
+        return NextResponse.json(
+            {
+                message: "Ha ocurrido un error al actualizar los luchadores",
+            },
+            {
+                status: 400,
+            }
+        );
+    }
 
     return NextResponse.json(
         {
-            message: 'Estamos montando este panel aún',
+            message: `Se han actualizado ${changedWrestlers.length} luchadores correctamente`,
         },
         {
             status: 200,
-        },
+        }
     );
 }
